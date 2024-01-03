9141ef1e9ff50a057649675b12a3eadd3971f0bc
Fahad <fsadakathussain@gmail.com>
2024-01-03 08:35:16 +0600

    model class added

--- main/xmlParserCsv.js
+++ main/xmlParserCsv.js
@@ -1,6 +1,17 @@
 import RssParser from 'rss-parser';
 import { createReadStream } from 'fs';
 import { parse } from 'csv-parse';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import 'dotenv/config';
+
+var awsConfig = {
+    region: `${process.env.DYNAMO_DB_REGION}`,
+    credentials: {
+        accessKeyId: `${process.env.ACCESS_KEY_ID}`,
+        secretAccessKey: `${process.env.SECRET_ACCESS_KEY}`
+    }
+}
 
 var xmlList = [];
 const rssparser = new RssParser({ preserveWhitespace: true });
@@ -17,17 +28,60 @@ csvParser.on('readable', function () {
 createReadStream(fileName).pipe(csvParser).on("end", function () {
     parsingUrl()
 });
-
+var motherObject = {};
 function parsingUrl() {
-    xmlList.slice(0, 5).forEach(async elem => {
-        await rssparser.parseURL(elem).then(feed => {
+    xmlList.slice(0, 10).forEach(async elem => {
+        await rssparser.parseURL(elem).then(async feed => {
+            var feedItemList = []
             feed.items.forEach(element => {
-                console.log(element.link)
+                feedItemList.push({
+                    title: element.title,
+                    articleUrl: element.link,
+                    pubDate: element.pubDate,
+                    summary: element.summary
+                })
             })
-        }).catch(function () { });
+            motherObject = {
+                title: feed.title,
+                companySite: feed.link,
+                articleTitle: feed.description,
+                image: feed.image,
+                innerItems: feedItemList
+            }
+        }).then(function () {
+        }).catch(function () {});
     })
 }
 
 
 
+
+
+var putDataToTable = (async () => {
+    const client = new DynamoDBClient(awsConfig);
+    const response = await client.send(
+        new PutCommand({
+            TableName: "list_feed",
+            Item: {
+                urls: "pk",
+                sort: "sk",
+                url_list: [
+                    {
+                        articleCount: 22,
+                        urlAdress: "http://www.abc929.com"
+                    },
+                    {
+                        articleCount: 21,
+                        urlAdress: "http://www.abc929.com"
+                    },
+                    {
+                        articleCount: 23,
+                        urlAdress: "http://www.abc929.com"
+                    }
+                ],
+                articleData: motherObject
+            }
+
+        }));
+})();
 
