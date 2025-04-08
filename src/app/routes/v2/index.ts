3de49edb993832f4eb2c78c6de3ce84b5fe19536
Fahad <fsadakathussain@gmail.com>
2025-04-08 23:40:12 +0600

    remove deprecated v2 routes, server setup, and configuration files

--- src/app/routes/v2/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application } from "express";
-
-function userRoutesV2(app: Application) {
-
-  app.get("/v2/categories", (req, res) => {
-    
-  })
-
-}
--- src/infrastructure/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userRoutes from '../app/routes/v1';
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-userRoutes(app)
-export default app;
